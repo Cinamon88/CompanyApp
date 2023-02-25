3bd4840d0a9148378335d0b17c6e79d2c12362aa
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-25 13:54:03 +0100

    check if chosen document change

--- test/api/departments/put.test.js
+++ test/api/departments/put.test.js
@@ -15,9 +15,11 @@ describe('PUT /api/departments', () => {
     });
 
     it('/:id should update chosen document and return success', async () => {
-        const res = await request(server).put('/api/departments/5d9f1140f10a81216cfd4408').send({ name: '#Department #1' });
+        const res = await request(server).put('/api/departments/5d9f1140f10a81216cfd4408').send({ name: '=#Department #1=' });
+        const updatedDepartment = await Department.findOne({ _id: '5d9f1140f10a81216cfd4408' });
         expect(res.status).to.be.equal(200);
         expect(res.body).to.not.be.null;
+        expect(updatedDepartment.name).to.be.equal('=#Department #1=');
     });
 
     after(async () => {
